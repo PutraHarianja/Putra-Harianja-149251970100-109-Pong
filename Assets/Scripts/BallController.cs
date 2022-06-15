ed520a75cfba5522e017b0ff3892ec63ea444c7c
PutraHarianja <putra.harianja007@gmail.com>
2022-06-15 10:50:44 +0700

    adding speed power up

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    public Vector2 resetPosition;
+
+    private Rigidbody2D rig;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // transform.position = transform.position + (new Vector3(1f, 0, 0) * Time.deltaTime);
+        // transform.Translate((new Vector3(1f, 0, 0) * Time.deltaTime));
+        //transform.Translate(speed * Time.deltaTime);
+    
+    }
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
+
+    }
+
+    public void ActivatePUSpeedUp(float magnitude)
+    {
+        rig.velocity *= magnitude;
+    }
+}
